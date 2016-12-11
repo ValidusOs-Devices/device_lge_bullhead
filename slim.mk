919424217dd2058c62a7a4f7e4d5d7cfa6861d65
Victor Lapin <victor@slimroms.org>
2016-12-11 17:15:02 +0300

    Slimify / Start with NPF26F fingerprint
    Change-Id: Ib954506a74dcb8f0e4252b039ad37934ccb3b049
    Signed-off-by: Victor Lapin <victor@slimroms.org>

---
+++ slim.mk
@@ -0,0 +1,46 @@
+#
+# Copyright 2015 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Sample: This is where we'd set a backup provider if we had one
+# $(call inherit-product, device/sample/products/backup_overlay.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := bullhead
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit some common Slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
+
+PRODUCT_DEVICE := bullhead
+PRODUCT_NAME := slim_bullhead
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Nexus 5X
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=bullhead \
+    BUILD_FINGERPRINT=google/bullhead/bullhead:7.1.1/NPF26F/3468883:user/release-keys \
+PRIVATE_BUILD_DESC="bullhead-user 7.1.1 NPF26F 3468883 release-keys"
