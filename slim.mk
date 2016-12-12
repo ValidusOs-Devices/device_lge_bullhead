6524ab11b20f7b9901343a4fb52ef60b89b00edc
Victor Lapin <victor@slimroms.org>
2016-12-12 08:53:34 +0300

    Update build fingerprint to NMF26F
    Signed-off-by: Victor Lapin <victor@slimroms.org>

--- slim.mk
+++ slim.mk
@@ -39,5 +39,5 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT=google/bullhead/bullhead:7.1.1/NPF26F/3468883:user/release-keys \
-PRIVATE_BUILD_DESC="bullhead-user 7.1.1 NPF26F 3468883 release-keys"
+    BUILD_FINGERPRINT=google/bullhead/bullhead:7.1.1/NMF26F/3425388:user/release-keys \
+    PRIVATE_BUILD_DESC="bullhead-user 7.1.1 NMF26F 3425388 release-keys"
