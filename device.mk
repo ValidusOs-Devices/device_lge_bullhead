0846a46f116b62af80f5424f744f7c792deade43
Jongrak Kwon <jongrak.kwon@lge.com>
2016-12-13 12:48:27 -0800

    bullhead: support fingerprint SW20.6
    - includes fingerprint build option
    - add sepolicy for SW20.6
    
    Bug: 33251689
    
    Change-Id: I1ec2df4375a06374af465484a264e594d5f61fb8

--- device.mk
+++ device.mk
@@ -494,3 +494,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 $(call inherit-product-if-exists, hardware/qcom/msm8994/msm8992.mk)
 $(call inherit-product-if-exists, vendor/qcom/gpu/msm8994/msm8994-gpu-vendor.mk)
+$(call inherit-product, vendor/lge/bullhead/fingerprint/device/device.mk)
