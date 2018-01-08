d264958f0f6da9edb7a3cdc9e47416fb9fb39e63
allianceSparx <alliancesparx@gmail.com>
2018-01-08 08:39:24 +0000

    Revert "Recovery: Use fstab.bullhead.full"
    This reverts commit 2e7c11246fdcfc0a3d554a5ea70fb26d076328c3.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,7 +137,7 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 # Build a separate vendor.img
 TARGET_COPY_OUT_VENDOR := vendor
 
-TARGET_RECOVERY_FSTAB = device/lge/bullhead/fstab.bullhead.full
+TARGET_RECOVERY_FSTAB = device/lge/bullhead/fstab.bullhead
 
 TARGET_RELEASETOOLS_EXTENSIONS := device/lge/bullhead
 
